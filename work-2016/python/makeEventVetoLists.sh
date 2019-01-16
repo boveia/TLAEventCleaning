64d2e0087938863ca903800e74fa3ee844c93c6a
Antonio Boveia <antonio.boveia@cern.ch>
2019-01-16 13:58:54 -0500

    2018 version of makeEventVetoLists

--- work-2016/python/makeEventVetoLists.sh
+++ work-2016/python/makeEventVetoLists.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-LISTOFRUNNUMBERS=./list-2016-runnumbers.txt
+LISTOFRUNNUMBERS=./list-run2-runnumbers.txt
 EVENTVETOLISTDIR=./data/event-veto-info
 
 ################################################################
 
 # set up some version of Athena
 if [ "$SITEROOT" == "" ]; then
-    # exact version doesn't matter
-    source /afs/cern.ch/atlas/software/dist/AtlasSetup/scripts/asetup.sh 20.1.4.8
+    # exact version doesn't matter?
+    source /afs/cern.ch/atlas/software/dist/AtlasSetup/scripts/asetup.sh 21.0.20
 fi
 
 # make the output directory
@@ -16,5 +16,5 @@ mkdir -p ${EVENTVETOLISTDIR}
 # look over the list of run numbers and do showEventVeto for each run
 for runno in `cat ${LISTOFRUNNUMBERS}`
 do
-python showEventVeto-2016.py -r $runno -e $runno >& ${EVENTVETOLISTDIR}/event-veto-$runno.txt
+python showEventVeto-2018.py -r $runno -e $runno >& ${EVENTVETOLISTDIR}/event-veto-$runno.txt
 done
