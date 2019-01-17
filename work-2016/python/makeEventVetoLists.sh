34107f6db63e048f89afd1e201e4d09aceccaac1
Antonio Boveia <antonio.boveia@cern.ch>
2019-01-17 17:48:57 -0500

    parallelize
    will need to migrate to prun

--- work-2016/python/makeEventVetoLists.sh
+++ work-2016/python/makeEventVetoLists.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 LISTOFRUNNUMBERS=./list-run2-runnumbers.txt
-EVENTVETOLISTDIR=./data/event-veto-info
+EVENTVETOLISTDIR=./data/event-veto-info-run2-4-10
+WORKDIR=`pwd`
+BATCHDIR=$WORKDIR/batch
 
 ################################################################
 
@@ -11,10 +13,23 @@ if [ "$SITEROOT" == "" ]; then
     source /afs/cern.ch/atlas/software/dist/AtlasSetup/scripts/asetup.sh 21.0.20
 fi
 
+# make the batch directory
+mkdir -p ${BATCHDIR}
+
 # make the output directory
 mkdir -p ${EVENTVETOLISTDIR}
+
 # look over the list of run numbers and do showEventVeto for each run
 for runno in `cat ${LISTOFRUNNUMBERS}`
 do
-python showEventVeto-2018.py -r $runno -e $runno >& ${EVENTVETOLISTDIR}/event-veto-$runno.txt
+    BATCHNAME="${BATCHDIR}/job_${runno}.sh"
+    echo "#!/bin/bash" > $BATCHNAME
+    echo "cd $WORKDIR" >> $BATCHNAME
+    echo "export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup" >> $BATCHNAME
+    echo "alias asetup='source $AtlasSetup/scripts/asetup.sh'" >> $BATCHNAME
+    echo "source /afs/cern.ch/atlas/software/dist/AtlasSetup/scripts/asetup.sh 21.0.20" >> $BATCHNAME
+    echo "python showEventVeto-2018.py -r $runno -e $runno >& ${EVENTVETOLISTDIR}/event-veto-$runno.txt" >> $BATCHNAME
+    chmod u+x $BATCHNAME
+    echo $BATCHNAME
+    #bsub -q 1nh $BATCHNAME
 done
